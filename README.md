860b5831f5cc0ab3a3b2b6a12ada338ff0bccce4
Aurel Bruno <aurelbruno06@gmail.com>
2020-02-22 21:02:50 +0100

    initial comiit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-red-contrib-translate
+
+This is the source of the npm package translate node for Nodered contrib translate node
---
+++ translate/translate.js
@@ -0,0 +1,33 @@
+module.exports = function (RED) {
+    "use strict";
+    const xlate = require("./xlate")(RED);
+
+
+    function TranslateNode(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+
+        node.inputfields = n.inputfields;
+        node.outputfields = n.outputfields;
+        node.aclrules = n.aclrules;
+
+        var valid = true;
+
+        if (valid) {
+            this.on('input', (msg, send, done) => {
+                xlate.applyXlateRules(msg, node, (err, msg) => {
+                    console.log("Return from applyXlateRules:",err," msg:",msg)
+                    if (err) {
+                        console.log("applyXlateRules return error")
+                        done(err);
+                    } else if (msg) {
+                        console.log("applyXlateRules return msg")
+                        send(msg);
+                        done();
+                    }
+                })
+            });
+        }
+    }
+    RED.nodes.registerType("translate", TranslateNode);
+};
